dd837a6aff91ea776b2a55ec4f039fda1ecf3a59
JuanEstrada <juancorrea621@gmail.com>
2024-03-23 00:39:33 -0500

    todo por hoy(23)

--- plataforma/src/components/Areas/Programacion/Programacion.jsx
+++ plataforma/src/components/Areas/Programacion/Programacion.jsx
@@ -1,10 +1,50 @@
 import "./Programacion.css"
+import Front from "../../../imgs/programacion-front.jpg"
+import Back from "../../../imgs/programacion-back.jpg"
+import FS from "../../../imgs/programacion-FS.jpg"
+import Button from 'react-bootstrap/Button';
 
-function Programacion () {
-    return(
-        <>
-        <h1>Programacion</h1>
-        </>
+function Programacion() {
+    return (
+        <main id="bodyProgramacion">
+            <h1>Programación</h1>
+
+            <div className="contenedorProgramacionFront">
+                <img className="imgProgramacion" src={Front} alt="front" />
+                <div className="contenedorDescripcionProgramacion">
+                    <h4>Frontend</h4>
+                    <p><strong>Titulación: </strong>Frontend developer</p>
+                    <p><strong>Duración: </strong> 2 años</p>
+                    <p><strong>Conocimientos previos: </strong>Sin conocimientos previos</p>
+                    <p><strong>Descripción: </strong>Curso de programacion Frontend en el cual aprenderas todas las tecnologias más demandadas por el mercado en el Frontend.</p>
+                    <Button className="btnInscribirse" variant="outline-light">Inscribirse</Button>{' '}
+                </div>
+            </div>        
+
+            <div className="contenedorProgramacionBack">
+                <img className="imgProgramacion" src={Back} alt="front" />
+                <div className="contenedorDescripcionProgramacion">
+                    <h4>Backend</h4>
+                    <p><strong>Titulación: </strong>Backend developer</p>
+                    <p><strong>Duración: </strong> 3 años</p>
+                    <p><strong>Conocimientos previos: </strong>Sin conocimientos previos</p>
+                    <p><strong>Descripción: </strong>Curso de programacion Backend en el cual aprenderas todas las tecnologias más demandadas por el mercado en el Backend.</p>
+                    <Button className="btnInscribirse" variant="outline-light">Inscribirse</Button>{' '}
+                </div>
+            </div>
+
+            <div className="contenedorProgramacionFullStack">
+                <img className="imgProgramacion" src={FS} alt="front" />
+                <div className="contenedorDescripcionProgramacion">
+                    <h4>Full Stack</h4>
+                    <p><strong>Titulación: </strong>Full Stack developer</p>
+                    <p><strong>Duración: </strong> 5 años</p>
+                    <p><strong>Conocimientos previos: </strong>Sin conocimientos previos</p>
+                    <p><strong>Descripción: </strong>Curso de programacion Full Stack donde aprenderas a combinar todas las tecnologias del Frontend y Backend para crear aplicaciones completas (Full Stack).</p>
+                    <Button className="btnInscribirse" variant="outline-light">Inscribirse</Button>{' '}
+                </div>
+            </div>
+        </main>
     )
 }
 
--- plataforma/src/components/Home/Home.jsx
+++ plataforma/src/components/Home/Home.jsx
@@ -5,17 +5,17 @@ function Home() {
         <main id="bodyHome">
             <div className="contenedorSectionHome">
                 <h2>Nuestra academia</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam delectus commodi similique debitis beatae adipisci dolorum totam impedit consectetur rem officia reprehenderit nemo, magnam ad? Magnam porro a temporibus officia!LoremloremLorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati assumenda, sed delectus alias odit illum quasi qui, dolore necessitatibus sint molestiae? Nihil, numquam ea quidem quam molestiae dolores labore iste.</p>
+                <p>Somos una academia que se impulsa por democratizar la educación, todo nuestro contenido es totalmente gratuito con certificaciones y de alta calidad, que esperas empezar a estudiar? este es tu momento, Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam delectus commodi similique debitis beatae adipisci dolorum totam impedit consectetur rem officia reprehenderit nemo, magnam ad? Magnam porro a temporibus officia!LoremloremLorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati assumenda, sed delectus alias odit illum quasi qui, dolore necessitatibus sint molestiae? Nihil, numquam ea quidem quam molestiae dolores labore iste.</p>
             </div>
 
             <div className="contenedorSectionRequisitos">
                 <h2>Requisitos para aplicar</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam delectus commodi similique debitis beatae adipisci dolorum totam impedit consectetur rem officia reprehenderit nemo, magnam ad? Magnam porro a temporibus officia!LoremloremLorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati assumenda, sed delectus alias odit illum quasi qui, dolore necessitatibus sint molestiae? Nihil, numquam ea quidem quam molestiae dolores labore iste.</p>
+                <p>No se necesita ningun requisito para estudiar en Educademi, solo necesitas las ganas y disposicion de aprender, Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam delectus commodi similique debitis beatae adipisci dolorum totam impedit consectetur rem officia reprehenderit nemo, magnam ad? Magnam porro a temporibus officia!LoremloremLorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati assumenda, sed delectus alias odit illum quasi qui, dolore necessitatibus sint molestiae? Nihil, numquam ea quidem quam molestiae dolores labore iste.</p>
             </div>
 
             <div className="contenedorSectionNovedades">
                 <h2>Novedades</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam delectus commodi similique debitis beatae adipisci dolorum totam impedit consectetur rem officia reprehenderit nemo, magnam ad? Magnam porro a temporibus officia!LoremloremLorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati assumenda, sed delectus alias odit illum quasi qui, dolore necessitatibus sint molestiae? Nihil, numquam ea quidem quam molestiae dolores labore iste.</p>
+                <p>Proximamente se abrira una nueva area, estar muy atentos a las ultimas novedades!  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam delectus commodi similique debitis beatae adipisci dolorum totam impedit consectetur rem officia reprehenderit nemo, magnam ad? Magnam porro a temporibus officia!LoremloremLorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati assumenda, sed delectus alias odit illum quasi qui, dolore necessitatibus sint molestiae? Nihil, numquam ea quidem quam molestiae dolores labore iste.</p>
             </div>
         </main>
     )
